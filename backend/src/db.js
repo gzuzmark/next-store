4c5acd36f72afe5425ac1a9631002d9dfa85a234
alonso <alonso.m144@gmail.com>
2019-12-13 18:04:23 -0500

    Backend support to Add, update and delete products

---
+++ backend/src/db.js
@@ -0,0 +1,11 @@
+// Enablkes connection to prisma remote db and also to query it
+const { Prisma } = require('prisma-binding');
+
+const db = new Prisma({
+  typeDefs: 'src/generated/prisma.graphql',
+  endpoint: process.env.PRISMA_ENDPOINT,
+  secret: process.env.PRISMA_SECRET,
+  debug: false,
+});
+
+module.exports = db;
